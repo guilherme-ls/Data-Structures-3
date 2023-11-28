d480443c61acbcfc8ee431e5ee1920e28e70310b
emanuelpg <emanuelpercinio@gmail.com>
2023-11-28 19:17:18 -0300

    Ajuste Funcionalidade 7

--- funcoes_arvore.h
+++ funcoes_arvore.h
@@ -14,7 +14,7 @@ void insere_raiz(FILE* arquivo, header_arvore* cabecalho, dado *data);
 dado* insert(FILE* arquivo, header_arvore* cabecalho, dado *data, int* altura);
 dado* insert_here(FILE* arquivo, header_arvore* cabecalho, dado* data, registro_arvore* reg, int pos, int pont);
 void clear_dado(dado* data);
-int pega_raiz(FILE* arq_arvore, header_arvore header, registro_arvore* reg)
+int pega_raiz(FILE* arq_arvore, header_arvore header, registro_arvore* reg);
 int ler_reg_dados_do_indice(FILE* arq_dados, registro_arvore reg_arvore, registro* reg_dados, int iBuscaBinaria);
 
 #endif
