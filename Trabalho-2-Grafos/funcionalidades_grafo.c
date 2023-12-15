a644f82975b7a04e78c483fc61a534c69d836af0
Guilherme Schmidt <guilherme.schmidt@usp.br>
2023-12-15 17:55:08 -0300

    template for 11

--- Trabalho-2-Grafos/funcionalidades_grafo.c
+++ Trabalho-2-Grafos/funcionalidades_grafo.c
@@ -109,6 +109,7 @@ void funcionalidade9() {
     fclose(arq_dados);
     libera_grafo(g);
 }
+
 void funcionalidade10() {
     char nome_dados[TAM_ARQ_LEITURA];
     int numero_buscas;
@@ -145,7 +146,7 @@ void funcionalidade10() {
             break;
         }
 
-        // insere aresta lida no grafo, caso valida
+        // insere aresta lida no grafo transposto, caso valida
         if(reg.tecnologiaOrigem.tamanho != 0 && reg.tecnologiaDestino.tamanho != 0)
             insere_aresta(&g, reg.tecnologiaDestino, 0, reg.tecnologiaOrigem, reg.grupo, reg.peso);
 
@@ -154,6 +155,7 @@ void funcionalidade10() {
         free(reg.tecnologiaDestino.nome);
     }
     
+    // recebe tecnologias e busca no grafo transposto, retornando suas arestas de saida
     for(int i = 0; i < numero_buscas; i++) {
         char tecnologia_buscada[TAM_ARQ_LEITURA];
         scan_quote_string(tecnologia_buscada);
@@ -165,9 +167,63 @@ void funcionalidade10() {
     fclose(arq_dados);
     libera_grafo(g);
 }
+
 void funcionalidade11() {
-    ;
+    char nome_dados[TAM_ARQ_LEITURA];
+    int numero_buscas;
+    scanf("%s", nome_dados);
+    scanf("%d", &numero_buscas);
+
+    // abre arquivo de entrada em modo de leitura
+    FILE *arq_dados;
+    if (open(&arq_dados, nome_dados, "rb")) {
+        // fim da execucao em caso de erros
+        return;
+    }
+
+    // le registro de cabecalho do arquivo de dados e retorna quaisquer erros
+    header cabecalho_dados;
+    int erro = check_cabecalho(arq_dados, &cabecalho_dados);
+    if (erro == 1) {
+        // fim da execucao em caso de erros
+        return;
+    }
+
+    registro reg;  // Registro de dados a ser empregrado na leitura
+
+    // cria e inicializa o grafo e seu transposto
+    grafo g;
+    grafo g_transposto;
+    inicializa_grafo(&g, cabecalho_dados.nroTecnologias);
+    inicializa_grafo(&g_transposto, cabecalho_dados.nroTecnologias);
+
+    //loop de leitura do arquivo de dados e criação do grafo
+    while(1) {
+        // funcao de leitura dos registros, retornando 1 quando encontra EOF
+        int end = ler_registro(arq_dados, &reg);
+        if (end) {
+            // break com fim do arquivo
+            break;
+        }
+
+        // insere aresta lida nos grafos, caso valida
+        if(reg.tecnologiaOrigem.tamanho != 0 && reg.tecnologiaDestino.tamanho != 0) {
+            insere_aresta(&g, reg.tecnologiaOrigem, reg.peso, reg.tecnologiaDestino, 0, reg.peso);
+            insere_aresta(&g_transposto, reg.tecnologiaDestino, 0, reg.tecnologiaOrigem, reg.grupo, reg.peso);
+        }
+
+        // libera variaveis alocadas
+        free(reg.tecnologiaOrigem.nome);
+        free(reg.tecnologiaDestino.nome);
+    }
+    
+
+    // fecha arquivo de dados e libera grafo
+    fclose(arq_dados);
+    libera_grafo(g);
+    libera_grafo(g_transposto);
 }
+
 void funcionalidade12() {
     ;
 }
